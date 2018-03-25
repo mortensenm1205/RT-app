d517d35067cfb1aab7f83c5313d329e864046f2e
Matt Mortensen <mortensenm1205@gmail.com>
2018-03-25 16:28:05 -0600

    got api working with express, now to add controller to format data coming in

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var movie = require('../server/config/rt.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+router.get('/', movie.getMoviesCurrentlyInTheaters)
 
 module.exports = router;
