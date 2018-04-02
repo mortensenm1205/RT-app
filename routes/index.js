bb669c8caa743b01095083a619eae50ca6a134cf
Matt Mortensen <mortensenm1205@gmail.com>
2018-04-02 17:56:50 -0600

    updated my current movie date to always update with new Date function. Removed model folder and now will send data straight to my route. Next will use controller files to send data to react components

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
-var movie = require('../server/controllers/currentMovies.js');
-
+var path = require('path');
+var movieData = require('../server/config/rt.js');
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
+router.get('/', function(req, res, next) {
+  movieData.getMoviesCurrentlyInTheaters().then((response) => {
+    let currentMovie = response.data.results;
+    let movieOveriew = {};
+    currentMovie.map((movie) => {
+      movieOveriew.title = movie.title;
+      movieOveriew.overview = movie.overview;
+      res.write(JSON.stringify(movieOveriew));
+    })
+    res.end();
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+});
 
 module.exports = router;
