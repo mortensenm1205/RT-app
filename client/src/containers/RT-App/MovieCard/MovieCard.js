55748f869a56f5b5620538006d9ad0d914f07535
Matt Mortensen <mortensenm1205@gmail.com>
2018-04-09 21:16:20 -0600

    finally got my movie card right, tried messing with image and displaying the poster, might take too long. Moving on to personal raiting system

--- client/src/containers/RT-App/MovieCard/MovieCard.js
+++ client/src/containers/RT-App/MovieCard/MovieCard.js
@@ -5,7 +5,7 @@ class MovieCard extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { data: [] };
+    this.state = { data: [], img: '' };
   }
 
   componentDidMount() {
@@ -20,19 +20,23 @@ class MovieCard extends Component {
       }
     })
       .then(res => res.json())
-      .then(data => this.setState({ data }))
+      .then(data => {
+        let img = data.poster_path.split('').splice(1).join('');
+        this.setState({ data, img })
+      })
   }
 
   render() {
 
     const { data } = this.state;
+    const { img } = this.state;
 
     return (
       <div>
         <h1>{data.title}</h1>
+        <img src={img} alt={img} />
         <p>{data.overview}</p>
         <div>Popularity: {data.popularity}</div>
-        <p>{data.homepage}</p>
       </div>
     )
   }
